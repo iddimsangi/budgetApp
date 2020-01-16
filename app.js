e2a72cbdafb324badedea42ff4bb3712c0c3e854
iddimsangi <adammsangi@gmail.com>
2020-01-16 13:37:38 +0300

    use keypress event to global Document

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ document.querySelector('.add__btn').addEventListener('click',function(){
     console.log('testing button clicking...');
 })
 
+document.addEventListener('keypress',function(event){
+    console.log(event);
+})
+//TO DO LIST AFTER THE BUTTON CLICKED...
+//.1 Get input field data
+
+
+
+//.2 Add the item to the budget controller
+
+
+
+//.3 Add the item to the UI
+
+
+
+//.4 Calculate the budget
+
+
+
+//.5 Display the budget on UI
 
 
 })(budgetController,UIcontroller);
