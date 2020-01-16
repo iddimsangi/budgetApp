3b8c3bf18a59dcdfb6492e21cf21625b13cd57a3
iddimsangi <adammsangi@gmail.com>
2020-01-16 14:19:38 +0300

    create a public method and shared from one module to the other

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var budgetController = (function(){
 })();
 
 var UIcontroller = (function(){
+//create a public method for share input field values
+    return{
+        getInputFields:function(){
+            return{
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                inputValue: document.querySelector('.add__value').value
+            }
+        }
+    }
+
 
 })();
 
 var controller = (function(budgetCtrl,uiCtrl){
     var ctrlAdd = function(){
-console.log('you have clicked the button');
+// console.log('you have clicked the button');
 //TO DO LIST AFTER THE BUTTON CLICKED...
 //.1 Get input field data
-
-
+var inputs = uiCtrl.getInputFields();
+console.log(inputs);
 
 //.2 Add the item to the budget controller
 
