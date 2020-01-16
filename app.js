bf86cbecfee5060687f6a4d9677d1b0a9609beb2
iddimsangi <adammsangi@gmail.com>
2020-01-16 13:19:46 +0300

    adding event listener to a click button

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ var UIcontroller = (function(){
 })();
 
 var controller = (function(budgetCtrl,uiCtrl){
+document.querySelector('.add__btn').addEventListener('click',function(){
+    console.log('testing button clicking...');
+})
+
+
 
 })(budgetController,UIcontroller);
