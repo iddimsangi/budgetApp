83c7e79ed08be6f115d362cc9e91aeb8b7ddd412
iddimsangi <adammsangi@gmail.com>
2020-01-17 12:25:19 +0300

    add the data to the UI

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 var budgetController = (function(){
+    var Expenses = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Incomes = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var data ={
+        allItems:{
+            exp:[],
+            inc:[]
+        },
+        totals:{
+            exp:0,
+            inc:0
+        }
+    };
+    return{
+        addItem:function(type,des,val){
+            var newItem,ID;
+        if(data.allItems[type].length > 0){
+            ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+        }else{
+            ID = 0;
+        }
+           
+            if(type === 'exp'){
+                newItem = new Expenses(ID,des,val);
+            }else if(type === 'inc'){
+                newItem = new Incomes(ID,des,val);
+            }
+            return data.allItems[type].push(newItem);
+            // data.allItems.exp/inc.push(newItem);
+           
+        },
+        testing:function(){
+            console.log(data);
+        }
+    }
 
 })();
+
+
 
 var UIcontroller = (function(){
 //create a private object for DOM Strings
@@ -8,7 +52,9 @@ var domStrings = {
         typeStrng:'.add__type',
         descriptionStrng:'.add__description',
         inputValueStrng:'.add__value',
-        inputBtnStrng:'.add__btn'
+        inputBtnStrng:'.add__btn',
+        expContainer:'.expenses__list',
+        incContainer:'.income__list'
 };
 
 //create a public method for share input field values
@@ -17,16 +63,41 @@ var domStrings = {
             return{
                 type: document.querySelector(domStrings.typeStrng).value,
                 description: document.querySelector(domStrings.descriptionStrng).value,
-                inputValue: document.querySelector(domStrings.inputValueStrng).value
-            }
+                value: document.querySelector(domStrings.inputValueStrng).value
+            };
         },
         //method that will make a private object to be public to the other controller
-       getDOMstring: function(){
+      
+
+        getDOMstring: function(){
             return domStrings;
-       } 
+       } ,
+       //method for retreive the data from DOM
+       addList:function(obj,type){
+           var html,newHtml,element;
+           //create HTML Strings with placeholders text
+           if(type === 'exp'){
+               element = domStrings.expContainer;
+html ='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+           }else if(type === 'inc'){
+               element = domStrings.incContainer;
+html ='<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+           }
+           //Replace the place holders text with some actual data
+           newHtml = html.replace('%id%', obj.id);
+           newHtml = newHtml.replace('%description%', obj.description);
+           newHtml = newHtml.replace('%value%', obj.value);
+
+           //Insert the HTML into the DOM
+           document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+       }
     }
 
 })();
+
+
+
 
 var controller = (function(budgetCtrl,uiCtrl){
   //structure and organize the codes
@@ -50,11 +121,11 @@ var inputs = uiCtrl.getInputFields();
 console.log(inputs);
 
 //.2 Add the item to the budget controller
-
+var newItem = budgetCtrl.addItem(inputs.type, inputs.description, inputs.value);
 
 
 //.3 Add the item to the UI
-
+uiCtrl.addList(newItem, inputs.type);
 
 
 //.4 Calculate the budget
